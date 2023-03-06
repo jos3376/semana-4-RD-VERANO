c8fbeec7191f4d820cdd3e0b79ec1a674bcd2491
JOSÈ RODRIGUEZ <112988259+jos3376@users.noreply.github.com>
2023-03-05 22:30:26 -0300

    Add files via upload

---
+++ semana 4/variables.js
@@ -0,0 +1,7 @@
+alert("probando variables de js");
+let marca ="Ford";
+let modelo = "Falcon";
+let año = "1983";
+let marca_y_modelo = marca + " " + modelo;
+console.log (marca,modelo,año)
+alert(marca_y_modelo);
